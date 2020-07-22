64ef4086fb66fef3a6ebd8261813974f75aa8608
Naomi Calabretta <me@arytonex.pw>
2020-07-22 04:44:57 +0200

    More useful testing app

--- test/index.js
+++ test/index.js
@@ -1,10 +1,11 @@
 "use strict";
 
-const glasstron = require('..');
-const electron = require('electron');
+const glasstron = require("..");
+const electron = require("electron");
+const path = require("path");
 
 electron.app.commandLine.appendSwitch("enable-transparent-visuals");
-electron.app.on('ready', () => {
+electron.app.on("ready", () => {
 	setTimeout(
 		spawnWindow,
 		process.platform == "linux" ? 1000 : 0
@@ -15,13 +16,26 @@ function spawnWindow(){
 	const win = new glasstron.BrowserWindow({
 		width: 800,
 		height: 600,
+		backgroundColor: "#00000000",
+		transparent: true,
+		title: "Glasstron test window",
+		webPreferences: {
+			nodeIntegration: true
+		}
 	});
 	
-	win.webContents.loadURL('about:blank');
-	win.webContents.insertCSS('html { background-color: transparent; }');
+	win.webContents.loadURL(`file://${__dirname}/index.html`);
 	
 	win.blurType = "blurbehind";
 	win.setBlur(true);
+	
+	electron.ipcMain.on("blurToggle", async (e, value) => {
+		const win = electron.BrowserWindow.fromWebContents(e.sender);
+		if(win !== null){
+			await win.setBlur(value);
+			e.sender.send("blurToggled", await win.getBlur());
+		}
+	});
 	
 	return win;
 }
