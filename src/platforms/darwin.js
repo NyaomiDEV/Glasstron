84d5c864e6de9568efee3470971633e786295afa
Eugene <e@ajenti.org>
2021-01-09 17:33:48 +0100

    fixed macOS vibrancy (fixes #55) (#58)

--- src/platforms/darwin.js
+++ src/platforms/darwin.js
@@ -40,8 +40,8 @@ module.exports = class Darwin extends Platform {
 	 */
 	static _wrapVibrancy(win, vibrancyInitialValue = "fullscreen-ui"){
 		// Wrap the original setVibrancy
-		const originalFunction = win.setVibrancy;
 		
+		const originalFunction = win.setVibrancy.bind(win);
 		// Set an array of two values
 		// [
 		//   Vibrancy value to be reported,
@@ -80,11 +80,8 @@ module.exports = class Darwin extends Platform {
 		});
 		
 		// Bind the new wrapped setVibrancy function and apply it
-		const boundFunction = ((vibrancy) => {this.vibrancy = vibrancy;}).bind(win);
-		Object.defineProperty(win, "setVibrancy", {
-			get: () => boundFunction
-		});
-		
+		win.setVibrancy = vibrancy => { win.vibrancy = vibrancy; };
+
 		// Now we need an exposed method to get the correct blur status
 		Object.defineProperty(win, "getBlur", {
 			get: () => _vibrancy[1] === null
@@ -93,9 +90,8 @@ module.exports = class Darwin extends Platform {
 		// And a correct method to set the blur status
 		Object.defineProperty(win, "setBlur", {
 			get: () => (bool) => {
-				if(bool)
-					_vibrancy[1] = _vibrancy[0]; // maybe there's a better way to do this
-				win.setVibrancy(bool ? win.vibrancy : null);
+				_vibrancy[1] = bool ? _vibrancy[0] : null;
+				originalFunction(_vibrancy[1]);
 			}
 		});
 	}
